1b3b48cf5ecf73d633130f9960b2286f28f41c82
Felix Morau <felixmorau@gmail.com>
2019-01-27 18:02:57 +0100

    added userList tab

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,12 @@
 import User from "./components/User/User.vue"
 import About from "./components/About/About.vue"
 import Projects from "./components/Projects/Projects.vue"
+import UserList from "./components/User/UserList.vue"
 
 
 export const routes = [
     { path: "/", component: About },
     { path: "/profile", component: User },
     { path: "/projects", component: Projects },
-
+    { path: "/members", component: UserList },
 ];
