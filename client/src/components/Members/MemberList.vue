8c7971449dfcc4aced43fcb905d954377211e90c
Felix Morau <felixmorau@gmail.com>
2019-02-02 18:16:56 +0100

    Changed file structure for memberList, changed Css, changed userBio.vue to accomondate changes, still need to fix with proper UX for sign in, sign up, userbio and reset password

---
+++ client/src/components/Members/MemberList.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <section>
+            <div class="innerDiv">
+                <div class="innerDiv">
+                <div v-masonry transition-duration="1s" item-selector=".memberCard">
+                  <div v-masonry-tile class="item" v-for="(member, index) in members" :key=index fit-width="true">
+                    <div class="memberCard" :style="{ 'background-image': 'url(' + member.image + ')' }">
+                    <div class="innerDiv">
+                        <div class="overlay">
+                          <h3 class="memberName">{{ member.displayName }}</h3>
+                          <h3 class="memberRole"> {{ member.role }}</h3>
+                          <i class="fab fa-github projectGithub" style="font-size: 75px;" @click="goToLink(member.github)"></i>
+                          <div class="projectDescription" @click="goToLink(member.website)">{{ member.website }}</div>
+                        </div>
+                    </div>
+                    </div>
+                    <a class="projects"></a>
+                  </div>
+                </div> 
+                </div>
+        </div>
+        </section>
+    </div>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    goToLink(link) {
+      window.open(link, "_blank");
+    }
+  },
+  props: ["members"]
+};
+</script>
+
+<style scoped>
+.memberCard {
+  width: 25%;
+  height: 250px;
+  margin: 0 auto;
+  cursor: pointer;
+  background-position: center top;
+  color: #fff;
+  transition: 1s;
+  font-size: 1.5vw;
+  overflow: hidden;
+}
+.outerDiv {
+  background: #0a949f;
+  background: -webkit-linear-gradient(to bottom, #ed5181, #009fff);
+  background: linear-gradient(to bottom, #ed5181, #009fff);
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.innerDiv {
+  min-height: 300px;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  /* padding-top: 150px;
+  padding-bottom: 150px; */
+}
+h2 {
+  margin: 0;
+  font-size: 7vw;
+}
+h3 {
+  margin: 0;
+}
+p {
+  margin: 0;
+}
+.projectGithub {
+  transition: 1s;
+}
+.overlay {
+  opacity: 0;
+  transition: 1s;
+  overflow: hidden;
+  background: -webkit-linear-gradient(
+    to bottom right,
+    rgba(255, 61, 37, 0.9) 50%,
+    rgba(255, 99, 72, 0.9)
+  );
+  background: linear-gradient(
+    to bottom right,
+    rgba(255, 61, 37, 0.9) 50%,
+    rgba(255, 99, 72, 0.9)
+  );
+  height: 100%;
+}
+.memberCard:hover .overlay {
+  opacity: 1;
+  transition: 1s;
+  margin: 0 auto;
+}
+.memberCard:hover {
+  color: #fff;
+  transition: 1s;
+}
+@media only screen and (max-width: 1000px) {
+  .memberCard {
+    width: 50%;
+    font-size: 15px;
+  }
+  .innerDiv {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .projectName {
+    font-size: 20px;
+  }
+}
+</style>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,12 @@
 import User from "./components/User/User.vue"
 import About from "./components/About/About.vue"
 import Projects from "./components/Projects/Projects.vue"
-import UserList from "./components/User/UserList.vue"
+import Member from "./components/Members/Member.vue"
 
 
 export const routes = [
     { path: "/", component: About },
     { path: "/profile", component: User },
     { path: "/projects", component: Projects },
-    { path: "/members", component: UserList },
+    { path: "/members", component: Member },
 ];
